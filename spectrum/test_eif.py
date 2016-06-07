41b699aaa39365c0d0afb060a6762c1628925fe0
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-07 12:54:54 +0000

    Testing 3 articles instead of 2
    Adding a POA

--- spectrum/test_eif.py
+++ spectrum/test_eif.py
@@ -1,18 +1,15 @@
-import unittest
+import pytest
 from spectrum import generator
 from spectrum import input
 from spectrum import checks
 
-class TestPublishing(unittest.TestCase):
-    def test_uploaded_article_gets_transformed_into_eif(self):
-        article = generator.article_zip()
-        input.production_bucket.upload(article.filename())
-        checks.eif.of(id=article.id())
-        checks.website.unpublished(id=article.id())
+def all_stored_articles():
+    return ["15853", "15893", "15600"]
 
-    def test_another_larger_article_gets_transformed_into_eif(self):
-        article = generator.article_zip(template_id="15853")
-        input.production_bucket.upload(article.filename())
-        checks.eif.of(id=article.id())
-
+@pytest.mark.parametrize("article", all_stored_articles())
+def test_uploaded_article_gets_transformed_into_eif(article):
+    article = generator.article_zip()
+    input.production_bucket.upload(article.filename())
+    checks.eif.of(id=article.id())
+    checks.website.unpublished(id=article.id())
 

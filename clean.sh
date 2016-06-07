c6800592d7daa333be770e30ba1aeead7b67334b
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-07 08:50:11 +0000

    Making scripts runnable from everywhere

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+# cd to the project's directory so that the script can be run from anywhere
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 source venv/bin/activate
 python -c "from spectrum.aws import clean; clean()"
--- execute.sh
+++ execute.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+# cd to the project's directory so that the script can be run from anywhere
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 source venv/bin/activate
 rm -f build/junit.xml
 py.test -v --junitxml build/junit.xml -s spectrum

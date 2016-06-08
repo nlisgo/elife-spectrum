a7b37cdef4f8d2ab794413b013e132e46b42f2fe
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-08 13:02:01 +0000

    Letting processes print even when executed by xdist in parallel

--- conftest.py
+++ conftest.py
@@ -1,4 +1,11 @@
+import sys
+
 import pytest
+
+# so that other processes run by xdist can still print
+# http://stackoverflow.com/questions/27006884/pytest-xdist-without-capturing-output
+# https://github.com/pytest-dev/pytest/issues/680
+sys.stdout = sys.stderr
 
 def pytest_addoption(parser):
     parser.addoption("--article-id", action="store", default=None,

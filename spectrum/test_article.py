3c8cb7a210611c52958242f488dc10bfc3fd60e5
Giorgio Sironi <g.sironi@elifesciences.org>
2016-06-08 14:11:46 +0000

    Image resizing comes logically before website publishing

--- spectrum/test_article.py
+++ spectrum/test_article.py
@@ -12,9 +12,9 @@ def test_article_flows_in_the_pipeline(template_id, article_id_filter):
     article = generator.article_zip(template_id)
     input.PRODUCTION_BUCKET.upload(article.filename())
     checks.EIF.of(id=article.id(), version=article.version())
-    checks.WEBSITE.unpublished(id=article.id(), version=article.version())
     for each in article.figure_names():
         checks.IMAGES.of(id=article.id(), figure_name=each, version=article.version())
     if article.has_pdf():
         checks.PDF.of(id=article.id(), version=article.version())
+    checks.WEBSITE.unpublished(id=article.id(), version=article.version())
 

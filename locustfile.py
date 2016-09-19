04c34757188236c53f206ad0e2579ee4984ab8c5
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-19 15:53:46 +0100

    Mega-hack to print stats to a file

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpLocust, TaskSet
 from bs4 import BeautifulSoup
+import gevent
 
 # http://demo--journal.elifesciences.org/content/4/e10627
 def article(l):
@@ -29,3 +30,19 @@ class WebsiteUser(HttpLocust):
     #min_wait=5000
     #max_wait=9000
 
+def stats_persist():
+    while True:
+        from locust.runners import locust_runner
+        print "stats_persist"
+        print locust_runner
+        store_stats('/tmp/locust-stats.log', locust_runner.request_stats)
+        gevent.sleep(2)
+
+def store_stats(filename, stats):
+    with open(filename, "w") as f:
+        f.write("path,method,num_requests,num_failures,min_response_time,max_response_time,avg_response_time\n")
+        for k in stats:
+            r = stats[k]
+            f.write("%s,%s,%d,%d,%d,%d,%d\n" % (r.name,r.method,r.num_requests,r.num_failures,r.min_response_time,r.max_response_time,r.avg_response_time));
+    
+gevent.spawn(stats_persist) 

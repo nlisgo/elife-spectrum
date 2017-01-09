429ee3ff6032a288def8b43b804a42caf67c0cbf
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-09 11:47:53 +0000

    Logging replacement and article id pair

--- spectrum/generator.py
+++ spectrum/generator.py
@@ -140,6 +140,7 @@ class ArticleZip:
 
     def replace_in_text(self, replacements):
         """Beware: violates immutability, as it modifies the file in place for performance reasons"""
+        LOGGER.info("Replacing %s in article", replacements, extra={'id': self._id})
         with zipfile.ZipFile(self._filename, 'w') as zip_file:
             for file in glob.glob(self._directory + "/*"):
                 if file.endswith('.xml'):

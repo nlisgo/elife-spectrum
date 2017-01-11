00089aff2b8031f22004ecf907e7541004e4a104
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-11 13:44:17 +0000

    Removing commented out skipped test

--- spectrum/test_article.py
+++ spectrum/test_article.py
@@ -15,8 +15,6 @@ def test_article_first_version(template_id, article_id_filter, generate_article)
     if article_id_filter:
         if template_id != article_id_filter:
             pytest.skip("Filtered out through the article_id_filter")
-    #if template_id == '00230':
-    #    pytest.skip("SKIPPING 00230 due to incomplete support for videos")
 
     article = generate_article(template_id)
     _ingest_and_publish(article)
@@ -74,14 +72,14 @@ def test_article_already_present_version(generate_article, version_article):
     error = checks.DASHBOARD.error(id=article.id(), version=1, run=2)
     assert re.match(r".*already published article version.*", error['event-message']), ("Error found on the dashboard does not match the expected description: %s" % error)
 
-#@pytest.mark.continuum
-#def test_searching_for_a_new_article(generate_article, modify_article):
-#    template_id = 15893
-#    invented_word = _invented_word()
-#    new_article = modify_article(generate_article(template_id), replacements={'cytomegalovirus':invented_word})
-#    _ingest_and_publish(new_article)
-#    result = checks.API.wait_search(invented_word)
-#    assert len(result['items']) == 1, "Searching for %s returned too many results: %d" % (invented_word, len(result['items']))
+@pytest.mark.continuum
+def test_searching_for_a_new_article(generate_article, modify_article):
+    template_id = 15893
+    invented_word = _invented_word()
+    new_article = modify_article(generate_article(template_id), replacements={'cytomegalovirus':invented_word})
+    _ingest_and_publish(new_article)
+    result = checks.API.wait_search(invented_word)
+    assert len(result['items']) == 1, "Searching for %s returned too many results: %d" % (invented_word, len(result['items']))
 
 def _ingest(article):
     input.PRODUCTION_BUCKET.upload(article.filename(), article.id())

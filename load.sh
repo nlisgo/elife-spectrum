071cf12daabc89a6cfedc20583153a761d736e56
Giorgio Sironi <g.sironi@elifesciences.org>
2017-01-24 09:37:15 +0000

    Storing some useful URLs for load testing

--- load.sh
+++ load.sh
@@ -4,4 +4,8 @@ set -e
 users=${1:-10}
 time=${2:-60}
 
-siege -c${users} -t${time}S http://end2end--journal.elifesciences.org/content/4/e10627
+#siege -c${users} -t${time}S https://prod--journal.elifesciences.org/content/4/e09560 # Homo Naledi, also 09561
+siege -c${users} -t${time}S https://prod--journal.elifesciences.org/content/4/e09560/figures # Homo Naledi, also 09561
+#siege -c${users} -t${time}S https://prod--journal.elifesciences.org/content/4/e10627
+#siege -c${users} -t${time}S https://prod--journal.elifesciences.org/content/4/e10627/figures
+#siege -c${users} -t${time}S https://prod--journal.elifesciences.org

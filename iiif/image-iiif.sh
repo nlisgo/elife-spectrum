01f6b61b4ff8aeb8c081f2b1aa677c99870e8fed
Alfred Pennyworth <elife-alfred-user@elifesciences.org>
2017-02-21 14:53:21 +0000

    Do not request a resize: some images are too small

--- iiif/image-iiif.sh
+++ iiif/image-iiif.sh
@@ -10,4 +10,4 @@ path=$1
 echo $path
 folder=images/$(echo $path | grep -o "^[0-9]\+")
 mkdir -p $folder
-wget -O "images/${path}.jpg" -c "https://ci--lax.elifesciences.org/iiif/$path/full/560,/0/default.jpg"
+wget -O "images/${path}.jpg" -c "https://ci--lax.elifesciences.org/iiif/$path/full/full/0/default.jpg"

248fd90222352e8d88edfc417625532bd19f6a67
Alfred Pennyworth <elife-alfred-user@elifesciences.org>
2017-02-21 12:19:43 +0000

    Improvement to IIIF testing scripts

--- iiif/image-iiif.sh
+++ iiif/image-iiif.sh
@@ -1,4 +1,13 @@
-echo $1
-folder=$(echo $path | grep -o "^[0-9]\+")
+#!/bin/bash
+set -e
+
+if [ "$#" -ne "1" ]; then
+    echo "Usage: $0 list_of_paths.txt"
+    exit 1
+fi
+
+path=$1
+echo $path
+folder=images/$(echo $path | grep -o "^[0-9]\+")
 mkdir -p $folder
-wget -O $path.jpg -c "https://ci--lax.elifesciences.org/iiif/$path/full/560,/0/default.jpg"
+wget -O "images/${path}.jpg" -c "https://ci--lax.elifesciences.org/iiif/$path/full/560,/0/default.jpg"

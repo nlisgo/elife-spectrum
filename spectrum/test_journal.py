2f072ab3bb8cdc475cfe09a6fc3eacf806e51297
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-09 13:28:58 +0000

    Added other semi-static pages

--- spectrum/test_journal.py
+++ spectrum/test_journal.py
@@ -19,18 +19,29 @@ def test_magazine():
 @pytest.mark.journal_cms
 def test_various_generic_pages():
     checks.JOURNAL.generic("/about")
+    checks.JOURNAL.generic("/about/early-career")
+    checks.JOURNAL.generic("/about/innovation")
+    checks.JOURNAL.generic("/about/openness")
+    checks.JOURNAL.generic("/about/peer-review")
     checks.JOURNAL.generic("/alerts")
+    checks.JOURNAL.generic("/annual-reports")
     checks.JOURNAL.generic("/archive/2016")
+    checks.JOURNAL.generic("/archive/2016")
+    checks.JOURNAL.generic("/articles/correction")
     checks.JOURNAL.generic("/community")
     checks.JOURNAL.generic("/contact")
     checks.JOURNAL.generic("/for-the-press")
     checks.JOURNAL.generic("/resources")
     checks.JOURNAL.generic("/terms")
+    #checks.JOURNAL.generic("/who-we-work-with")
 
 #    path: /content/{volume}/e{id}.bib
 #    path: /content/{volume}/e{id}.ris
 #    path: /articles/{type}
 
+#path: /download/{uri}/{name}
+
+#path: /about/people/{type}
 #    path: /collections
 # follow 1 link
 #    path: /events/{id}
@@ -42,6 +53,10 @@ def test_various_generic_pages():
 #    path: /podcast
 # follow 1 link
 #    path: /subjects
+# follow 1 link
+#path: /inside-elife
+# follow 1 link
+#path: /articles/{type}
 # follow 1 link
 
 #    path: /interviews/{id}
